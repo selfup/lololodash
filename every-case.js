b3c4b32202aea11ea4b3332270b73743277a3ccf
selfup <boudinot.regis@yahoo.com>
2015-11-23 15:03:16 -0700

    every-case - ex3

---
+++ every-case.js
@@ -0,0 +1,15 @@
+var _ = require('lodash');
+
+var worker = function(cities) {
+  return _.forEach(cities, function(city) {
+    if (city.population > 1) {
+      city.size = 'big';
+    } else if (city.population > 0.5 && city.population < 1) {
+      city.size = 'med';
+    } else {
+      city.size = 'small';
+    };
+  });
+};
+
+module.exports = worker;
