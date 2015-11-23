fe4ff6c5507fdd111b72133419713663d3ee546a
selfup <boudinot.regis@yahoo.com>
2015-11-23 15:24:20 -0700

    taking a break

--- every-case.js
+++ every-case.js
@@ -1,9 +1,11 @@
+// var pry = require('pryjs')
 var _ = require('lodash');
 
 var worker = function(cities) {
   return _.forEach(cities, function(city) {
     if (city.population > 1) {
       city.size = 'big';
+      eval(pry.it)
     } else if (city.population > 0.5 && city.population < 1) {
       city.size = 'med';
     } else {
---
+++ everyone-is-min.js
@@ -0,0 +1,7 @@
+var _ = require("lodash");
+
+var worker = function(towns) {
+  return _.forEach()
+};
+
+module.exports = worker;
